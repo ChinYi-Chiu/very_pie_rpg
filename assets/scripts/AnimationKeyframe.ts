7b2df50a0fd9d1eadf9f3a6b8bf92ae153e92d2a
Yue <yue806161@gmail.com>
2024-02-01 15:31:13 +0800

    戰鬥場景結尾

--- assets/scripts/AnimationKeyframe.ts
+++ assets/scripts/AnimationKeyframe.ts
@@ -1,13 +1,25 @@
-import { _decorator, Component, director, find, Animation, Sprite, SpriteFrame, resources  } from 'cc';
+import { _decorator, Component, director, find, Animation, Node  } from 'cc';
+import { TextShower } from "./TextShower";
 const { ccclass, property  } = _decorator;
+
 
 @ccclass('AnimationKeyframe')
 export class AnimationKeyframe extends Component {
 
-    @property(Sprite)
-    public Tozy_sprite: Sprite = null;
-    @property(Sprite)
-    public Chii_sprite: Sprite = null;
+    @property(TextShower)
+    public textShower: TextShower;
+    @property(Node)
+    public Tozy_fight: Node = null;
+    @property(Node)
+    public Chii_fight: Node = null;
+    @property(Node)
+    public Tozy_failed: Node = null;
+    @property(Node)
+    public Chii_victory: Node = null;
+    @property(Node)
+    public Chair: Node = null;
+    @property(Node)
+    public Sack: Node = null;
 
     private playAnimation(nodePath: string, animationName: string) {
         const bodyNode = find(nodePath);
@@ -19,32 +31,32 @@ export class AnimationKeyframe extends Component {
         }
     }
     
-    private changeSprite(sprite:Sprite,newSpritePath: string) {
-        // 從資源管理器中加載新的 SpriteFrame
-        resources.load(newSpritePath, SpriteFrame, (err, spriteFrame: SpriteFrame) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            // 更換 Sprite 組件的圖片
-            if (sprite) {
-                sprite.spriteFrame = spriteFrame;
-            }
-        });
+    private setActive(node: Node, isActive: boolean) {
+        if (node) {
+            node.active = isActive;
+        }
     }
 
     //ScenesTransition
     onTransitionFinished() {
         director.loadScene('FightScene');
     }
-    xxx(){
+    BleedAnime_End(){
         director.loadScene('EndingA');
     }
 
     //???
     BleedAnime_ChangeSprite(){
-        this.changeSprite(this.Tozy_sprite, `image/tozy/failed`);
-        this.changeSprite(this.Chii_sprite, `image\超哥\failed`);
+
+        this.textShower.showText = "起哥:古人說過，忍無可忍，無須再忍。超派!!!!!!";
+        this.textShower.OnShowTextOneByOne();
+
+        this.setActive(this.Tozy_failed, true);
+        this.setActive(this.Tozy_fight, false);
+        this.setActive(this.Chii_victory, true);
+        this.setActive(this.Chii_fight, false);
+        this.setActive(this.Chair, false);
+        this.setActive(this.Sack, false);
     }
 
 }
