33b82dd5eeaefb27e7e86d1c91c08e47e0b6c5cf
maru <t108360758@ntut.org.tw>
2024-02-05 11:44:09 +0800

    更改Animation Controller 啟動及定義動畫監聽器的方式(by json)

--- assets/scripts/AnimationController.ts
+++ assets/scripts/AnimationController.ts
@@ -1,68 +1,122 @@
-import { _decorator, Animation, AnimationClip, Component, Event, Node, director } from 'cc';
+import { _decorator, Animation, AnimationClip, JsonAsset, Component, Event, Node, director } from 'cc';
 import { GameManager } from './GameManager';
 import { AudioController } from './AudioController';
 import { BattleManager_test } from './BattleManager_test';
 const { ccclass, property } = _decorator;
 
+
+/**
+ * 動畫類型結構
+ * @description 
+ * 後面有 ? 的為選填項，除了選填項其他請務必一定要填入值
+ */
+interface IAnimation {
+    animate_storage: (INormal | IFood | ITrans )[]
+}
+interface INormal { //事件(動畫)
+    type: "normal",
+    node_id: string,//animationcontroller中的property
+    controller_id: string, 
+    animation_id?:string,
+    ringtone_id?: string 
+}
+interface IFood { //事件(動畫)
+    type: "food",
+    node_id: string,
+    controller_id: string, 
+    animation_id?:string,
+    ringtone_id?: string
+}
+
+interface ITrans { //事件(動畫)
+    type: "trans",
+    node_id: string,
+    controller_id: string, 
+    animation_id?:string,
+    ringtone_id?: string,
+    trans_scene: string
+}
+
 @ccclass('AnimationController')
 export class AnimationController extends Component {
+
+    // JSON 動畫文本
+    @property(JsonAsset)
+    private jsonAnimate: JsonAsset | null = null;
+    private animate: IAnimation | null = null;
+
     start() {
-        // 開始撥放 start 這個動畫
-        this.gameManager?.node.on('Start', (ev: Event) => {
-            this.start_page.play("Indoor_SceneTrans"); //播動畫
-            this.audioController.play("startPage"); //播音樂
-            setTimeout(() => { //等動畫播完後做其他事
-                this.audioController.stop("startPage"); //音樂停
-                this.gameManager.updateUI(); // 播完動畫，進入下一個場景
-            }, this.start_page.getState("Indoor_SceneTrans").duration * 1000);
-        }, this);
+    
+        if (this.jsonAnimate) this.animate = this.jsonAnimate.json as IAnimation;
 
-        // 開始播放超哥走進來的動畫。
-        this.gameManager?.node.on('chiiComing', (ev: Event) => {
-            this.chii.play("chiiComing"); //播動畫
-            this.audioController.play("chiiWalking"); //播音樂
-            setTimeout(() => { //等動畫播完後做其他事
-                this.audioController.stop("chiiWalking"); //音樂停
-                this.gameManager.updateUI(); // 播完動畫，進入下一個場景
-            }, this.chii.getState("chiiComing").duration * 1000);
-        }, this);
-
-        // 開始播放anan走出去的動畫。
-        this.gameManager?.node.on('girlwalkaway', (ev: Event) => {
-            this.girlwalkaway.play("girlwalkaway"); //播動畫
-            this.audioController.play("chiiWalking"); //播音樂
-            setTimeout(() => { //等動畫播完後做其他事
-                this.audioController.stop("chiiWalking"); //音樂停
-                this.gameManager.updateUI(); // 播完動畫，進入下一個場景
-            }, this.girlwalkaway.getState("girlwalkaway").duration * 1000);
-        }, this);
-
-        // 開始播放anan走進來的動畫。
-        this.gameManager?.node.on('girlwalkin', (ev: Event) => {
-            this.girlwalkin.play("girlwalkin"); //播動畫
-            this.audioController.play("chiiWalking"); //播音樂
-            setTimeout(() => { //等動畫播完後做其他事
-                this.audioController.stop("chiiWalking"); //音樂停
-                this.gameManager.updateUI(); // 播完動畫，進入下一個場景
-            }, this.girlwalkin.getState("girlwalkin").duration * 1000);
-        }, this);
-
-        // 開始撥放 Indoor_SceneTrans_End 這個動畫
-        this.gameManager?.node.on('Indoor_SceneTrans_End', (ev: Event) => {
-            this.end.play("Indoor_SceneTrans_End");//播動畫
-            setTimeout(() => { //等動畫播完後做其他事
-                director.loadScene('FightScene');//轉換到戰鬥場景
-            }, this.end.getState("Indoor_SceneTrans_End").duration * 1000);
-        }, this);
+        const animation_count=this.animate.animate_storage.length;
+        console.log("動畫事件數量:",animation_count);
+        console.log("所有動畫類型",this.animate.animate_storage);
+        for (let counter = 0; counter < animation_count; counter++) {
+            let animate_storage = this.animate.animate_storage[counter];
+            let l_type="";
+            let l_node_id="";
+            let l_controller_id="";
+            let l_animation_id="";
+            let l_ringtone_id="";
+            let l_trans_scene="";
+            l_type = animate_storage.type;
+            l_node_id = animate_storage.node_id;
+            l_controller_id = animate_storage.controller_id;
+            
+            if (animate_storage.animation_id) {
+                l_animation_id = animate_storage.animation_id;
+            } else {
+                l_animation_id = l_controller_id;
+            }
+        
+            if (animate_storage.ringtone_id) {
+                l_ringtone_id = animate_storage.ringtone_id;
+            } else {
+                l_ringtone_id = l_controller_id;
+            }
+            if (l_type === "trans") {
+                if ((animate_storage as ITrans).trans_scene) {
+                    l_trans_scene = (animate_storage as ITrans).trans_scene;
+                    console.log("建置場景轉換動畫事件監聽器：",l_node_id," 用於轉換至：",l_trans_scene);
+                }
+            }else{
+                console.log("建置動畫事件監聽器：",l_node_id);
+            }
+        
+            if (l_type === "normal") {
+                this.gameManager?.node.on(l_controller_id, (ev: Event) => {
+                    this[l_node_id]?.play(l_animation_id); //播動畫
+                    this.audioController?.play(l_ringtone_id); //播音樂
+                    setTimeout(() => {
+                        this.audioController?.stop(l_ringtone_id); //音樂停
+                        this.gameManager?.updateUI(); // 播完動畫，進入下一個場景
+                    }, this[l_node_id]?.getState(l_animation_id)?.duration * 1000 || 0);
+                }, this);
+            } else if (l_type === "trans") {
+                this.gameManager?.node.on(l_controller_id, (ev: Event) => {
+                    this[l_node_id]?.play(l_animation_id); //播動畫
+                    this.audioController?.play(l_ringtone_id); //播音樂
+                    setTimeout(() => {
+                        this.audioController?.stop(l_ringtone_id); //音樂停
+                        //this.gameManager?.updateUI(); // 播完動畫，進入下一個場景
+                        director.loadScene(l_trans_scene); //轉換到戰鬥場景
+                        console.log("轉換場景：",l_trans_scene);
+                    }, this[l_node_id]?.getState(l_animation_id)?.duration * 1000 || 0);
+                }, this);
+            }
+        }
         
     }
 
     update(deltaTime: number) {
 
     }
-
+ 
+    //手動增加node
     @property(GameManager)
     gameManager: GameManager | null = null;
+
     @property(AudioController)
     audioController: AudioController | null = null;
 
